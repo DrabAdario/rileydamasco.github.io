ad50ed21adfe770da26fa69305593f3a69fbc907
Riley Damasco <123606477+DrabAdario@users.noreply.github.com>
2025-02-12 17:00:18 -0500

    added a gif to projects for testing

--- Quinnipiac/IntershipStuff/NewWeb/my-simple-website/src/App.js
+++ Quinnipiac/IntershipStuff/NewWeb/my-simple-website/src/App.js
@@ -11,7 +11,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/rileydamasco.github.io">
       <div className="App">
         <Header />
         <main className="App-content">
--- Quinnipiac/IntershipStuff/NewWeb/my-simple-website/src/pages/Projects.js
+++ Quinnipiac/IntershipStuff/NewWeb/my-simple-website/src/pages/Projects.js
@@ -18,16 +18,22 @@ function Projects() {
   ];
 
   return (
+    
     <section className="projects">
+  <img src="my-simple-website/public/Assets/astro.gif" alt="AstroGame" width="250" />
+
       <h2>Projects</h2>
+
       {projects.map((project, index) => (
         <div key={index} className="project-card">
+
           <h3>{project.title}</h3>
           <p>{project.description}</p>
           <a href={project.link} target="_blank" rel="noopener noreferrer">
             View Project
           </a>
         </div>
+
       ))}
     </section>
   );
